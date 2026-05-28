e76d9bedc21c6dc8d5eff388f5a7dc1f47bc2209
HemannB <brunohemann@gmail.com>
2026-05-28 00:54:07 -0300

    get '/'say hello, world!
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- api.js
+++ api.js
@@ -4,4 +4,8 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.get('/', (request, response) => {
+    response.send('Hello, World!');
+});
+
 app.listen(PORT, () => console.log(`Servidor subiu na porta ${PORT}`));
